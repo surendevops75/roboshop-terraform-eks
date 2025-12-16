b174dc39d46d892bd3fb97cd41ed028047cefd6f
surendevops75 <surendevops75@gmail.com>
2025-12-16 14:19:25 +0530

    roboshop-terraform-eks

---
+++ 90-eks/main.tf
@@ -0,0 +1,58 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0" # module version
+
+  name               = local.common_name_suffix
+  kubernetes_version = "1.32"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+    metrics-server = {}
+  }
+
+  # Optional
+  endpoint_public_access = false
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.private_subnet_ids
+  control_plane_subnet_ids = local.private_subnet_ids
+  create_node_security_group = false
+  create_security_group      = false
+  node_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    blue = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+     
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+    }
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+}
