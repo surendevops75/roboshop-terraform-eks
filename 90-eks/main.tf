849c0d5830c0ee488c156e5d59e5731806d5a09c
surendevops75 <surendevops75@gmail.com>
2025-12-17 09:08:57 +0530

    roboshop-terraform-eks

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -37,7 +37,7 @@ module "eks" {
     blue = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
+      instance_types = ["t3.micro"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
