4fe76f69bee72820be198dc94d79d3ec1f333a92
surendevops75 <surendevops75@gmail.com>
2025-12-18 15:49:45 +0530

    roboshop-terraform-eks

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -1,10 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0" # this is module version
+  version = "~> 21.0" # module version
 
   name               = local.common_name_suffix
-  # kubernetes_version = "1.33"
-  kubernetes_version = var.eks_version
+  kubernetes_version = "1.32"
 
   addons = {
     coredns                = {}
@@ -18,7 +17,10 @@ module "eks" {
     metrics-server = {}
   }
 
+  # Optional
   endpoint_public_access = false
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = local.vpc_id
@@ -29,62 +31,21 @@ module "eks" {
   node_security_group_id = local.eks_node_sg_id
   security_group_id = local.eks_control_plane_sg_id
 
+
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     blue = {
-      create = var.enable_blue
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      kubernetes_version = var.eks_nodegroup_blue_version
       instance_types = ["m5.xlarge"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
       }
-      
-      # cluster nodes autoscaling
+     
       min_size     = 2
       max_size     = 10
       desired_size = 2
-
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
-
-      labels = {
-        nodegroup = "blue"
-      }
-    }
-
-    green = {
-      create = var.enable_green
-      ami_type       = "AL2023_x86_64_STANDARD"
-      kubernetes_version = var.eks_nodegroup_green_version
-      instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
-
-      labels = {
-        nodegroup = "green"
-      }
     }
   }
 
@@ -94,5 +55,4 @@ module "eks" {
         Name = local.common_name_suffix
     }
   )
-  
 }
