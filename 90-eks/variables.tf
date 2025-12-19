b3008cf18f3c8feccba89154908ad4064187d0bb
surendevops75 <surendevops75@gmail.com>
2025-12-19 09:02:54 +0530

    roboshop-terraform-eks

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -14,22 +14,22 @@ variable "domain_name" {
   default = "surendevops.fun"
 }
 
-variable "eks_version" {
+# variable "eks_version" {
 
-}
+# }
 
-variable "eks_nodegroup_blue_version" {
+# variable "eks_nodegroup_blue_version" {
 
-}
+# }
 
-variable "eks_nodegroup_green_version" {
+# variable "eks_nodegroup_green_version" {
 
-}
+# }
 
-variable enable_blue {
+# variable enable_blue {
 
-}
+# }
 
-variable enable_green {
+# variable enable_green {
     
-}
+# }
